fc8455cbd2a37f1c5af84434f8c0cf0e7f1e684e
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-06-19 04:32:14 +0000

    FEAT: built Temperature and Details  component

--- src/components/TemparatureAndDetails.js
+++ src/components/TemparatureAndDetails.js
@@ -23,23 +23,47 @@ const TemparatureAndDetails = () => {
         />
         <p className="text-5xl">34°</p>
         <div className="flex flex-col space-y-2">
-
           <div className="flex font-light text-sm items-center justify-center">
-            <UilTemperature size={18} className='mr1' />
+            <UilTemperature size={18} className="mr1" />
             Real fell:
             <span className="font-medium ml-1">32°</span>
           </div>
           <div className="flex font-light text-sm items-center justify-center">
-            <UilTear size={18} className='mr1' />
+            <UilTear size={18} className="mr1" />
             Humidity:
             <span className="font-medium ml-1">65%</span>
           </div>
           <div className="flex font-light text-sm items-center justify-center">
-            <UilWind size={18} className='mr1' />
+            <UilWind size={18} className="mr1" />
             Wind:
             <span className="font-medium ml-1">11 km/h</span>
           </div>
         </div>
+      </div>
+      <div className="flex flex-row items-center justify-center space-x-2 text-white text-sm py-3">
+        <UilSun />
+        <p className="font-light">
+          Rise: <span className="font-medium ml-1">06:45 AM</span>
+        </p>
+        <p className="font-light">|</p>
+
+        <UilSunset />
+        <p className="font-light">
+          Set: <span className="font-medium ml-1">07:35 PM</span>
+        </p>
+        <p className="font-light">|</p>
+
+        <UilSun />
+        <p className="font-light">
+          High: <span className="font-medium ml-1">45°</span>
+        </p>
+        <p className="font-light">|</p>
+
+        <UilSun />
+        <p className="font-light">
+          Low: <span className="font-medium ml-1">40°</span>
+        </p>
+       
       </div>
     </div>
   );
