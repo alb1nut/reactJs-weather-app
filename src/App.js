a066b462df3c159cd1daa36b742db08c89305faf
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-06-20 04:00:22 +0000

    FEAT: built weather api functions to be calling data from api

--- src/App.js
+++ src/App.js
@@ -5,8 +5,17 @@ import Inputs from './components/Inputs';
 import TimeAndLocation from './components/TimeAndLocation';
 import TemparatureAndDetails from './components/TemparatureAndDetails';
 import Forcast from './components/Forcast';
+import getWeatherData from './utils/weatherUtils';
 
 function App() {
+
+const fetchWeather = async () =>{
+  const data = await getWeatherData('weather' , {q:'Accra'});
+  console.log(data);
+}
+fetchWeather()
+
+
   return (
  
  <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
--- src/utils/weatherUtils.js
+++ src/utils/weatherUtils.js
@@ -1,2 +1,49 @@
-const API_KEY ='8e558e919fe8ca38089961a28828856d';
-const BASEURL ='https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}'
+const API_KEY = "8e558e919fe8ca38089961a28828856d";
+const BASEURL = "https://api.openweathermap.org/data/2.5";
+
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(BASEURL + "/" + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
+  return fetch(url).then((res) => res.json());
+  // .then((data) => data);
+};
+
+// export default getWeatherData;
+const formatCurrentWeather = (data) => {
+  const {
+    coord: { lat, lon },
+    main: { temp, feels_like, temp_min, temp_max, humidity },
+    name,
+    dt,
+    sys: { country, sunrise, sunset },
+    weather,
+    wind: { speed },
+  } = data;
+
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    humidity,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    weather,
+    speed,
+  };
+};
+
+const getFormattedWeatherData = async (searchParams) => {
+  const formattedCurrentWeather = await getWeatherData(
+    "weather",
+    searchParams
+  ).then(formatCurrentWeather);
+};
