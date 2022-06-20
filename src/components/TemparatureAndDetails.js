38ac6fd44eca3ab0ea92b45e072ad78da5b80c7d
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-06-20 05:36:38 +0000

    FEAT: finished inputing temparature data in UI

--- src/components/TemparatureAndDetails.js
+++ src/components/TemparatureAndDetails.js
@@ -70,13 +70,13 @@ const TemparatureAndDetails = ({
 
         <UilArrowUp />
         <p className="font-light">
-          High: <span className="font-medium ml-1">45°</span>
+          High: <span className="font-medium ml-1">{`${temp_max.toFixed()}°`}</span>
         </p>
         <p className="font-light">|</p>
 
         <UilArrowDown />
         <p className="font-light">
-          Low: <span className="font-medium ml-1">40°</span>
+          Low: <span className="font-medium ml-1">{`${temp_min.toFixed()}°`}</span>
         </p>
       </div>
     </div>
