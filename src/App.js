fc7fd22be5d3408f001a73db46b8c98a951eabbb
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-06-20 07:27:19 +0000

    FEAT: installed react toastify

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Forcast from "./components/Forcast";
 import getFormattedWeatherData from "./utils/weatherUtils";
 import { useEffect, useState } from "react";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   const [query, setQuery] = useState({ q: "accra" });
   const [units, setUnits] = useState("metric");
@@ -15,7 +18,10 @@ function App() {
 
   useEffect(() => {
     const fetchWeather = async () => {
-      await getFormattedWeatherData({...query, units}).then((data) => {
+      const message =query.q ? query.q : 'current location.'
+
+      toast.info('fetching weather for ' + message)
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
         setWeather(data);
       });
     };
@@ -23,26 +29,31 @@ function App() {
     fetchWeather();
   }, [query, units]);
 
-  const formatBackground =() =>{
-    if (!weather ) return "from-cyan-700 to-blue-700"
-    const threshold = units === 'metric' ? 20 : 60;
-    if (weather.temp <= threshold) return "from-cyan-700 to-blue-700"
+  const formatBackground = () => {
+    if (!weather) return "from-cyan-700 to-blue-700";
+    const threshold = units === "metric" ? 20 : 60;
+    if (weather.temp <= threshold) return "from-cyan-700 to-blue-700";
 
-    return 'from-yellow-700 to-orange-700' 
-  }
+    return "from-yellow-700 to-orange-700";
+  };
 
   return (
-    <div className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}>
-      <TopButtons setQuery={setQuery}/>
+    <div
+      className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}
+    >
+      <TopButtons setQuery={setQuery} />
       <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
       {weather && (
         <>
           <TimeAndLocation weather={weather} />
-          <TemparatureAndDetails  weather={weather}/>
-          <Forcast title="hourly forcast"  items={weather.hourly}/>
-          <Forcast title="daily forcast" items={weather.daily}/>
+          <TemparatureAndDetails weather={weather} />
+          <Forcast title="hourly forcast" items={weather.hourly} />
+          <Forcast title="daily forcast" items={weather.daily} />
         </>
       )}
+
+
+    <ToastContainer autoClose={10000} theme='colored' newestOnTop={true}/>
     </div>
   );
 }
