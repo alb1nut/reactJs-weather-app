01d65755450d555e46c83f97433e03e0561065ee
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-07-05 15:04:34 +0000

    made temparature and detail  component responsive

--- src/components/TemparatureAndDetails.js
+++ src/components/TemparatureAndDetails.js
@@ -30,26 +30,26 @@ const TemparatureAndDetails = ({
       <div className="flex items-center justify-center py-6 text-xl text-cyan-300">
         <p>{details}</p>
       </div>
-      <div className="flex flex-row items-center justify-between text-white py-3">
+      <div className="flex flex-row items-center mr-14  justify-between text-white py-3">
         <img
           src={iconUrlFromCode(icon)}
           alt=""
-          className="w-20"
+          className="sm:w-12 md:w-20"
         />
-        <p className="text-5xl">{`${temp.toFixed()}°`}</p>
+        <p className="sm:text-lg md:text-5xl">{`${temp.toFixed()}°`}</p>
         <div className="flex flex-col space-y-2">
-          <div className="flex font-light text-sm items-center justify-center">
-            <UilTemperature size={18} className="mr1" />
+          <div className="flex font-light text-sm items-center justify-center ">
+            <UilTemperature size={18} className="mr-1" />
             Real fell:
             <span className="font-medium ml-1">{`${feels_like.toFixed()}°`}</span>
           </div>
           <div className="flex font-light text-sm items-center justify-center">
-            <UilTear size={18} className="mr1" />
+            <UilTear size={18} className="mr-1" />
             Humidity:
             <span className="font-medium ml-1">{`${humidity.toFixed()}%`}</span>
           </div>
           <div className="flex font-light text-sm items-center justify-center">
-            <UilWind size={18} className="mr1" />
+            <UilWind size={18} className="mr-1" />
             Wind:
             <span className="font-medium ml-1">{`${speed.toFixed()}km/h`}</span>
           </div>
