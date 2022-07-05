fc19fbfac8eec8889d5aeab56380a8a5ed00147b
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-07-05 14:50:52 +0000

    made top input component responsive

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -32,7 +32,7 @@ if (units !==selectedUnit) setUnits(selectedUnit)
   }
 
   return (
-    <div className="flex flex-row justify-center my-6">
+    <div className="flex flex-row justify-center flex-wrap my-6">
       <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
         <input
           value={city}
